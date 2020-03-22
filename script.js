e4465f114bf4e57d037d277a6934f06a51067b2c
Ana Medrano <amedrano.fernandez@gmail.com>
2020-03-22 15:01:07 -0700

    First attempt at collecting user answers when clicked

--- script.js
+++ script.js
@@ -1,37 +1,71 @@
-var question = document.querySelector("#question");
-var image = document.querySelector("#image");
+var questionEl = document.querySelector("#question");
+var imageEl = document.querySelector("#image");
+var answersEl = document.querySelector("#answers");
 
-var answers0 = document.querySelector("#a0");
-var answers1 = document.querySelector("#a1");
-var answers2 = document.querySelector("#a2");
-var answers3 = document.querySelector("#a3");
+var answers = [a0El, a1El, a2El, a3El];
+var a0El = document.querySelector("#a0");
+var a1El = document.querySelector("#a1");
+var a2El = document.querySelector("#a2");
+var a3El = document.querySelector("#a3");
+var outcomeEl = document.querySelector("#outcome");
+
+// User Selection
+answersEl.addEventListener("click", function(event){
+    event.preventDefault();
+    var answer = event.target;
+    console.log(answer);
+
+})
 
 
 
-var questionBank = [
-    {
-        q: "Which direction is this person facing?",
-        a: ["Forward", "Backward", "To the left", "To the right"],
-        correct: 3, url: "https://i.picsum.photos/id/1025/4951/3301.jpg"
-    }
 
-]
 
-var i = 0;
-image.setAttribute = ("src", "https://i.picsum.photos/id/1014/6016/4000.jpg");
-question.textContent = questionBank[i].q;
-answers0.textContent = questionBank[i].a[0];
-answers1.textContent = questionBank[i].a[1];
-answers2.textContent = questionBank[i].a[2];
-answers3.textContent = questionBank[i].a[3];
 
-// function questionDisplay() {
-//     for (let i = 0; i < questionBank.length; i++) {
-//         image.setAttribute = ("src", "'" + questionBank[i].a[4] + "'";
-//         question.textContent = questionBank[i].q;
-//         answers0.textContent = questionBank[i].a[0];
-//         answers1.textContent = questionBank[i].a[1];
-//         answers2.textContent = questionBank[i].a[2];
-//         answers3.textContent = questionBank[i].a[3];
-//     }
+
+
+// var question = 0;
+
+
+// var questionBank = [
+//     {
+//         q: "Which direction is this person facing?",
+//         a: ["Backward", "To the left", "To the right", "What person?"],
+//         correct: 2, url: "https://i.picsum.photos/id/1025/4951/3301.jpg"
+//     },
+//     {
+//         q: "How old is this woman?",
+//         a: ["Backward", "To the left", "To the right", "What person?"],
+//         correct: 2, url: "https://i.picsum.photos/id/1025/4951/3301.jpg"
+//     },
+
+//     {
+//         q: "What is love?",
+//         a: ["Backward", "To the left", "To the right", "What person?"],
+//         correct: 2, url: "https://i.picsum.photos/id/1025/4951/3301.jpg"
+//     },
+
+// ]
+
+// function displayQuestions() {setInterval ( function displayQs(questionBank) {
+//     question.textContent = questionBank[question].q;
+//     answers0.textContent = questionBank[question].a[0];
+//     answers1.textContent = questionBank[question].a[1];
+//     answers2.textContent = questionBank[question].a[2];
+//     answers3.textContent = questionBank[question].a[3];
+//     question++
+
+// }, 3000);
+
 // }
+
+// displayQuestions();
+
+// // function displayQs(questionBank) {
+// //     question.textContent = questionBank[question].q;
+// //     answers0.textContent = questionBank[question].a[0];
+// //     answers1.textContent = questionBank[question].a[1];
+// //     answers2.textContent = questionBank[question].a[2];
+// //     answers3.textContent = questionBank[question].a[3];
+
+// // }
