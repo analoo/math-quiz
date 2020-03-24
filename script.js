0ecf60f4844cd2e1a6a085d8ea145d60f71cff99
Ana Medrano <amedrano.fernandez@gmail.com>
2020-03-24 15:44:22 -0700

    completed logic to display leaderboard

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 
 // pull all important data elements from the page
 var mainEl = document.querySelector("main");
-var questionEl = document.querySelector("#question");
-var answersEl = document.querySelector("#answers");
-var outcomeEl = document.querySelector("#outcome");
+
 var startEl = document.querySelector("#start");
 var timerEl = document.querySelector("#timer");
 var timerBoxEl = document.querySelector("#time-box")
 var challengeEl = document.querySelector("#challenge")
-var buttonsArray = document.querySelectorAll("button");
+var buttonsArray = document.querySelectorAll("button")
 
+var questionEl = document.querySelector("#question");
+var answersEl = document.querySelector("#answers");
+var outcomeEl = document.querySelector("#outcome");
 
-
+// ** Declare starting conditions ***
 var question = 0;
 var time = 60;
 var score = 0;
 var userAnswers = [];
-var scoresList = [{Ana: 100}, {Mark: 250}]
+var scoresList = [{name: "Ana", score: 100}, {name: "Mark", score: 250}];
+console.log(scoresList); 
+// = JSON.parse(localStorage.getItem("highscores"));
 var questionBank = [];
 
+// creates start for the game
+startEl.addEventListener("click", function () {
+    event.preventDefault();
+    startEl.style.display = "none";
+    challengeEl.style.display = "none";
+    timerBoxEl.style.display = "inline";
+    questionGenerator();
+    startTimer(time);
+    mainEl.style.display = "inline";
+    displayQuestions();
+
+})
 // make a question generator that creates math problems.
 
 function questionGenerator(){
@@ -50,19 +65,6 @@ function questionGenerator(){
 
 }
 
-startEl.addEventListener("click", function () {
-    event.preventDefault();
-    startEl.style.display = "none";
-    challengeEl.style.display = "none";
-    timerBoxEl.style.display = "inline";
-    questionGenerator();
-    startTimer(time);
-    mainEl.style.display = "inline";
-    displayQuestions();
-
-})
-
-
 function startTimer() {
     var timeRemaining = setInterval(function () {
         timerEl.textContent = (time - 1);
@@ -79,6 +81,16 @@ function startTimer() {
 
 }
 
+function displayQuestions() {
+    questionBank = questionGenerator();
+     questionEl.textContent = questionBank.q;
+     buttonsArray[1].textContent = questionBank.a[0];
+     buttonsArray[2].textContent = questionBank.a[1];
+     buttonsArray[3].textContent = questionBank.a[2];
+     buttonsArray[4].textContent = questionBank.a[3];
+ 
+ }
+
 answersEl.addEventListener("click", function (event) {
     event.preventDefault();
     var answer = event.target.id;
@@ -91,17 +103,6 @@ answersEl.addEventListener("click", function (event) {
     console.log(question);
 
 });
-
-
-function displayQuestions() {
-   questionBank = questionGenerator();
-    questionEl.textContent = questionBank.q;
-    buttonsArray[1].textContent = questionBank.a[0];
-    buttonsArray[2].textContent = questionBank.a[1];
-    buttonsArray[3].textContent = questionBank.a[2];
-    buttonsArray[4].textContent = questionBank.a[3];
-
-}
 
 function checkAnswers(answer) {
     if (questionBank.correct == answer.charAt(1)){
@@ -118,17 +119,30 @@ function checkAnswers(answer) {
 }
 
 function storeScore(name){
-    scoresList.append({name:score});
+    scoresList.append({name: score});
     localStorage.setItem("highScores", JSON.stringify(scoresList));
 }
-console.log(score);
 
 function displayScores(){
-    var leaderBoard = JSON.parse(localStorage.getItem("highscores"));
+   
 
-
+    
+    for (let i = 0; i < scoresList.length; i++){
+        var tbodyEl = document.querySelector("tbody");
+        var tableRowEl = document.createElement("tr");
+        var dataEl1 = document.createElement("td");
+        var dataEl2 = document.createElement("td");
+        dataEl1.textContent = scoresList[i].name;
+        dataEl2.textContent = scoresList[i].score;
+        tbodyEl.append(tableRowEl);
+        tableRowEl.append(dataEl1);
+        tableRowEl.append(dataEl2);
+    }
 
 }
+displayScores();
+
+console.log(scoresList); 
 
 function gameOver(){
     document.querySelector("#leaderboard").style.display = "inline";
