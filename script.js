baa4e995a40666ced034fd8b0edcbe1d101d696c
Ana Medrano <amedrano.fernandez@gmail.com>
2020-03-23 23:40:58 -0700

    changed quiz concept from being a Captcha joke (are you a robot) to a math quiz. Created a random question generator and modified existing code to fit new logic.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // pull all important data elements from the page
 var mainEl = document.querySelector("main");
 var questionEl = document.querySelector("#question");
-var imageEl = document.querySelector("#image");
 var answersEl = document.querySelector("#answers");
 var a0El = document.querySelector("#a0");
 var a1El = document.querySelector("#a1");
@@ -11,92 +10,59 @@ var a3El = document.querySelector("#a3");
 var outcomeEl = document.querySelector("#outcome");
 var startEl = document.querySelector("#start");
 var timerEl = document.querySelector("#timer");
+var timerBoxEl = document.querySelector("#time-box")
+var challengeEl = document.querySelector("#challenge")
+
+
 
 
 
 var question = 0;
-var time = 100;
+var time = 10;
 var score = 0;
 var userAnswers = [];
+var scores = [{Ana: 100}, {Mark: 250}]
+var questionBank = []
 
-var questionBank = [
-    {
-        q: "Which direction is this woman facing?",
-        a: ["Backward", "To the left", "To the right", "What woman"],
-        correct: a2,
-        url: "https://i.picsum.photos/id/1014/6016/4000.jpg"
-    },
-    {
-        q: "How old is this woman?",
-        a: ["10 - 20 years old", "21 - 30 years old", "31 + years", "What woman?"],
-        correct: a3,
-        url: "https://i.picsum.photos/id/1025/4951/3301.jpg"
-    },
+// make a question generator that creates math problems.
 
-    {
-        q: "How many AC units are in this building??",
-        a: ["Less than 3", "4 - 6", "7 - 10", "11+" ],
-        correct: a0,
-        url: "https://i.picsum.photos/id/1054/3079/1733.jpg"
-    },
+function questionGenerator(){
+    var num1 = Math.floor(Math.random() * 50);
+    var num2 = Math.floor(Math.random() * 25);
+    var q = num1 + " + " + num2;
+    var options = [];
+    var answer = num1 + num2;
+    var answerIndex
+    // options.push(answer);
 
-    {
-        q: "How are these humans walking on clouds?",
-        a: ["They are angels", "Photoshop", "It's a lake", "What humans?"],
-        correct: a2,
-        url: "https://i.picsum.photos/id/1056/3988/2720.jpg"
-    },
+    while (options.length < 4){
+        var tempAns = num1 + Math.floor(Math.random() * Math.floor(25));
+        if (options.indexOf(tempAns) === -1){
+            options.push(tempAns);
+        }
+    }
 
-    {
-        q: "Strawberries are my favorite fruit",
-        a: ["Backward", "To the left", "To the right", "What person?"],
-        correct: a2,
-        url: "https://i.picsum.photos/id/1080/6858/4574.jpg"
-    },
+    if (options.indexOf(answer) === -1){
+        answerIndex = Math.floor(Math.random() * 4);
+        options[answerIndex] = answer;
 
-    {
-        q: "Which direction is this person facing?",
-        a: ["Backward", "To the left", "To the right", "What person?"],
-        correct: a2,
-        url: "https://i.picsum.photos/id/1025/4951/3301.jpg"
-    },
+    }
 
-    {
-        q: "Which direction is this person facing?",
-        a: ["Backward", "To the left", "To the right", "What person?"],
-        correct: a2,
-        url: "https://i.picsum.photos/id/1025/4951/3301.jpg"
-    },
+    else if (options.indexOf(answer) > -1){
+        answerIndex = options.indexOf(answer) ;
+    }
+    
+    questionBank.push({q: q, a: options, correct: answerIndex})
 
-    {
-        q: "Which direction is this person facing?",
-        a: ["Backward", "To the left", "To the right", "What person?"],
-        correct: a2,
-        url: "https://i.picsum.photos/id/1025/4951/3301.jpg"
-    },
-
-    {
-        q: "Which direction is this person facing?",
-        a: ["Backward", "To the left", "To the right", "What person?"],
-        correct: a2,
-        url: "https://i.picsum.photos/id/1025/4951/3301.jpg"
-    },
-
-    {
-        q: "Which direction is this person facing?",
-        a: ["Backward", "To the left", "To the right", "What person?"],
-        correct: a2,
-        url: "https://i.picsum.photos/id/1025/4951/3301.jpg"
-    },
-
-
-]
-
-// To start the game
+}
 
 startEl.addEventListener("click", function () {
     event.preventDefault();
-    document.querySelector("#start").style.display = "none";
+    startEl.style.display = "none";
+    challengeEl.style.display = "none";
+    timerBoxEl.style.display = "inline";
+    questionGenerator();
+    console.log("The question bank has: " + questionBank)
     startTimer(time);
     mainEl.style.display = "inline";
     displayQuestions();
@@ -110,10 +76,11 @@ function startTimer() {
         time--;
         console.log(time);
 
-        if (time == 0) {
+        if (time <= 0) {
             clearInterval(timeRemaining);
             mainEl.style.display = "none";
             console.log("You failed!");
+            storeScore();
 
         }
     }, 1000)
@@ -122,6 +89,7 @@ function startTimer() {
 
 answersEl.addEventListener("click", function (event) {
     event.preventDefault();
+    questionGenerator();
     var answer = event.target.id;
     checkAnswers(question,answer);
     console.log(checkAnswers);
@@ -135,7 +103,6 @@ answersEl.addEventListener("click", function (event) {
 
 
 function displayQuestions() {
-    imageEl.src = questionBank[question].url;
     questionEl.textContent = questionBank[question].q;
     a0El.textContent = questionBank[question].a[0];
     a1El.textContent = questionBank[question].a[1];
@@ -144,16 +111,22 @@ function displayQuestions() {
 }
 
 function checkAnswers(question, answer) {
-    if (questionBank[question].correct == answer){
+    console.log(answer.charAt(1));
+    if (questionBank[question].correct == answer.charAt(1)){
         score +=1
+        outcomeEl.textContent = "Correct!"
     }
 
-    else if ( question.correct != answer){
-        score -=1;
+    else if (question.correct != answer){
         time -=5;
+        outcomeEl.textContent = "Wrong"
+
     }
 
 }
 
+function storeScore(){
+    localStorage.setItem("highScores", score);
+}
 console.log(score);
 
