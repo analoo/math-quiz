e84e4e3b20836ceffa3a9690146e71bfba557289
Ana Medrano <amedrano.fernandez@gmail.com>
2020-03-24 22:41:01 -0700

    set leaderboard to be ordered

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 var mainEl = document.querySelector("main");
 
 var startEl = document.querySelector("#start");
+var restartEl = document.querySelector("#restart");
 var submitEl = document.querySelector("#submit");
 
 
@@ -17,20 +18,22 @@ var answersEl = document.querySelector("#answers");
 var outcomeEl = document.querySelector("#outcome");
 
 // ** Declare starting conditions ***
-var question = 0;
 var time = 60;
 var score = 0;
-var userAnswers = [];
 var correct;
 var scoresList = [];
 
-if(JSON.parse(localStorage.getItem("highScores"))==null){
+if (JSON.parse(localStorage.getItem("highScores")) == null) {
     localStorage.setItem("highScores", JSON.stringify(scoresList));
 }
 
-else{
+else {
     scoresList = JSON.parse(localStorage.getItem("highScores"))
 }
+
+
+
+
 
 renderScores();
 
@@ -66,7 +69,7 @@ function startTimer() {
 }
 
 // make a question generator that creates math problems.
-function questionGenerator(){
+function questionGenerator() {
     var num1 = Math.floor(Math.random() * 50);
     var num2 = Math.floor(Math.random() * 25);
     var q = num1 + " + " + num2;
@@ -74,40 +77,42 @@ function questionGenerator(){
     var answer = num1 + num2;
     var answerIndex;
 
-    while (options.length < 4){
+    while (options.length < 4) {
         var tempAns = num1 + Math.floor(Math.random() * Math.floor(25));
-        if (options.indexOf(tempAns) === -1){
+        if (options.indexOf(tempAns) === -1) {
             options.push(tempAns);
         }
     }
 
-    if (options.indexOf(answer) === -1){
+    if (options.indexOf(answer) === -1) {
         answerIndex = Math.floor(Math.random() * 4);
         options[answerIndex] = answer;
     }
 
-    else if (options.indexOf(answer) > -1){
-        answerIndex = options.indexOf(answer) ;
+    else if (options.indexOf(answer) > -1) {
+        answerIndex = options.indexOf(answer);
     }
 
     questionEl.textContent = q;
-    buttonsArray[1].textContent = options[0];
-    buttonsArray[2].textContent = options[1];
-    buttonsArray[3].textContent = options[2];
-    buttonsArray[4].textContent = options[3];
+    buttonsArray[2].textContent = options[0];
+    buttonsArray[3].textContent = options[1];
+    buttonsArray[4].textContent = options[2];
+    buttonsArray[5].textContent = options[3];
     correct = answerIndex;
 }
 
 answersEl.addEventListener("click", function checkAnswers(event) {
     event.preventDefault();
     var answer = event.target.id;
-    if (correct == answer.charAt(1)){
-        score +=1
+    if (correct == answer.charAt(1)) {
+        score += 1
+        outcomeEl.style.color = "green";
         outcomeEl.textContent = "Correct!"
     }
 
-    else{
-        time -=5;
+    else {
+        time -= 5;
+        outcomeEl.style.color = "red";
         outcomeEl.textContent = "Wrong"
 
     }
@@ -116,34 +121,50 @@ answersEl.addEventListener("click", function checkAnswers(event) {
 });
 
 
-function gameOver(){
+function gameOver() {
     document.querySelector("#leaderboard").style.display = "inline";
     timerBoxEl.style.display = "none";
     challengeEl.style.display = "inline";
     challengeEl.textContent = "GAME OVER. Your final scores is: " + score;
-    submitEl.addEventListener("click", function(event){
+    submitEl.addEventListener("click", function (event) {
         event.preventDefault();
         var initials = nameSpan.value;
         nameSpan.value = "";
         storeScore(initials)
+        document.querySelector("#leaderboard").style.display = "none";
+        restartEl.style.display = "inline";
+
+
     })
+
+    restartEl.addEventListener("click", function (event) {
+        event.preventDefault();
+        reSetGame();
+        challengeEl.textContent = "Think you have what it takes to prove you are a real mathlete?"
+    })
+
 }
 
-function storeScore(name){
-    scoresList.push({"name": name, "score": score});
+function storeScore(name) {
+    scoresList.push({ "name": name, "score": score });
     localStorage.setItem("highScores", JSON.stringify(scoresList));
     clearScoreDispay();
     renderScores();
 }
 
-function clearScoreDispay(){
+function clearScoreDispay() {
     var trEl = document.querySelector("tbody")
-    trEl.innerHTML ='';
+    trEl.innerHTML = '';
 }
 
-function renderScores(){ 
+function renderScores() {
     document.querySelector("#leader-list").style.display = "inline";
-    for(let i = 0; i < scoresList.length; i++){
+
+    // borrowed this scores sort from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    scoresList.sort(function (a, b) {
+        return b.score - a.score;
+      })
+    for (let i = 0; i < scoresList.length; i++) {
         var tbodyEl = document.querySelector("tbody")
         var tableRowEl = document.createElement("tr");
         var dataEl1 = document.createElement("td");
@@ -157,6 +178,24 @@ function renderScores(){
 
 }
 
-console.log("7:11")
+function reSetGame() {
+    restartEl.style.display = "none";
+    timerBoxEl.display = "none";
+    mainEl.style.display = "none";
+    startEl.style.display = "inline";
+    challengeEl.style.display = "inline";
+    time = 60;
+    score = 0;
+
+    if (JSON.parse(localStorage.getItem("highScores")) == null) {
+        localStorage.setItem("highScores", JSON.stringify(scoresList));
+    }
+
+    else {
+        scoresList = JSON.parse(localStorage.getItem("highScores"))
+    }
+
+}
+
 
 
